b3678cf5f6fc3281232a179f8c6a6fdcf717f0f2
pdr3392 <pchenr1que1@gmail.com>
2021-12-15 01:40:51 -0300

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function Header() {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        dashgo
+      </Text>
+    </Flex>
+  );
+}

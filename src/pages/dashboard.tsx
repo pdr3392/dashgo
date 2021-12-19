d558ef8f3b0a324c53b52d307ed639c904eccd38
pdr3392 <pchenr1que1@gmail.com>
2021-12-19 04:04:14 -0300

    responsive 2

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -66,7 +66,7 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da Semana
             </Text>
@@ -74,7 +74,7 @@ export default function Dashboard() {
             <Chart type="area" height={160} options={options} series={series} />
           </Box>
 
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
